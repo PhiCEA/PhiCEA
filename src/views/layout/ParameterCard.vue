82e571ea5d88d460262d77e952b5aba508ea7f8e
ixuejie <iixuejie@qq.com>
2025-05-23 16:05:17 +0800

    refactor(layout): 优化模型参数卡片显示效果

--- src/views/layout/ParameterCard.vue
+++ src/views/layout/ParameterCard.vue
@@ -1,14 +1,16 @@
 <template>
   <n-card title="模型参数" :bordered="false" size="large" aria-modal="true">
-    <template v-if="jobs.currentJob?.parameters">
-      {{ JSON.stringify(jobs.currentJob?.parameters, null, 2) }}
-    </template>
+    <n-scrollbar v-if="jobs.currentJob?.parameters" max-h-lg trigger="none">
+      <pre>
+        {{ JSON.stringify(jobs.currentJob?.parameters, null, 16) }}
+      </pre>
+    </n-scrollbar>
     <n-empty v-else></n-empty>
   </n-card>
 </template>
 
 <script setup lang="ts">
-import { NCard, NEmpty } from "naive-ui";
+import { NCard, NScrollbar, NEmpty } from "naive-ui";
 import { useJobStore } from "@/stores/job";
 
 const jobs = useJobStore();
