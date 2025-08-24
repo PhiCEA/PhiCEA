097be69b54dc97c9a36071e3c20792bbc02f156f
ixuejie <iixuejie@qq.com>
2025-08-25 06:22:24 +0800

    feat(layout): 使用 vue-json-pretty 格式化显示 JSON 数据
    - 在 package.json 中添加 vue-json-pretty 依赖
    - 在 ParameterCard 组件中使用 vue-json-pretty 替代手动 JSON.stringify
    - 引入 vue-json-pretty 的样式和类型定义

--- src/views/layout/ParameterCard.vue
+++ src/views/layout/ParameterCard.vue
@@ -1,9 +1,7 @@
 <template>
   <n-card title="模型参数" :bordered="false" size="large" aria-modal="true">
     <n-scrollbar v-if="jobs.currentJob?.parameters" max-h-lg trigger="none">
-      <pre>
-        {{ JSON.stringify(jobs.currentJob?.parameters, null, 16) }}
-      </pre>
+      <vue-json-pretty :data="(jobs.currentJob?.parameters as JSONDataType)" />
     </n-scrollbar>
     <n-empty v-else></n-empty>
   </n-card>
@@ -12,6 +10,9 @@
 <script setup lang="ts">
 import { NCard, NScrollbar, NEmpty } from "naive-ui";
 import { useJobStore } from "@/stores/job";
+import VueJsonPretty from "vue-json-pretty";
+import "vue-json-pretty/lib/styles.css";
+import { JSONDataType } from "vue-json-pretty/types/utils";
 
 const jobs = useJobStore();
 </script>
